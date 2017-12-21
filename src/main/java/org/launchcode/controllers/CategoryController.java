46a04a9817b3e314ccd555374b544a55081d917f
JT - LC101 <joetopp@gmail.com>
2017-12-21 14:45:51 -0600

    creating add category handler and view

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,10 +1,16 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Category;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("category")
@@ -19,5 +25,24 @@ public class CategoryController {
         model.addAttribute("title", "Categories");
 
         return "category/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    private String add(Model model) {
+        model.addAttribute(new Category());
+        model.addAttribute("title", "Add Category");
+
+        return "category/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    private String add(Model model, @ModelAttribute @Valid Category category, Errors errors) {
+
+        if(errors.hasErrors()) {
+            return "category/add";
+        } else {
+            categoryDao.save(category);
+            return "redirect:";
+        }
     }
 }
